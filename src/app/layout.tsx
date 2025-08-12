ec5cf01c190559a5c02d65d230c340a6cda7384a
Samir Sen <121782740+NotAScratch@users.noreply.github.com>
2025-08-12 16:07:59 +0000

    Remove unused font imports and related code from layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,9 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+// import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import ClientLayoutWrapper from "../components/ClientLayoutWrapper";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Lenika Maharjan | Creative Portfolio",
